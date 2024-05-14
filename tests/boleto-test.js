9f93c3926d5de41cc8fb08dd3c43fac6e6ed353f
kelton <kelton.fonseca@infosistemas.com.br>
2024-05-14 14:39:04 -0300

    Atualização de versão

--- tests/boleto-test.js
+++ tests/boleto-test.js
@@ -85,12 +85,12 @@ module.exports = {
 			test.done();
 		},
 
-		'Deve lançar exceção se as datas estiverem além de 2024': function(test) {
+		'Deve lançar exceção se as datas estiverem além de 2999': function(test) {
 			test.throws(function() {
 				Datas.novasDatas()
-					.comDocumento(1, 1, 2024)
-					.comVencimento(1, 1, 2024)
-					.comProcessamento(1, 1, 2024);
+					.comDocumento(1, 1, 2999)
+					.comVencimento(1, 1, 2999)
+					.comProcessamento(1, 1, 2999);
 			});
 
 			test.done();
